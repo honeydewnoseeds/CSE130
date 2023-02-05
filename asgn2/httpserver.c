d2ea280e7d2229242841577b75e3ff5f76ed1461
Charlotte Liang <yliang82@ucsc.edu>
2023-02-05 04:10:15 -0800

    echo server done

--- asgn2/httpserver.c
+++ asgn2/httpserver.c
@@ -6,6 +6,47 @@
 #include <arpa/inet.h>
 #include "asgn2_helper_funcs.h"
 
+#define MAX_BUF 4096
+
 int main(int argc, char **argv) {
+    Listener_Socket socket_fd;
+    //struct sockaddr_in server_addr;
+    int port;
+    char buf[MAX_BUF];
+    
+    // checking usage
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+    // getting port number
+    port = atoi(argv[1]);
+        // if port if not between 1 and 65535
+    if (port < 1 || 65535 < port){
+        fprintf(stderr, "Invalid Port\n");
+        return 1;
+    }    
+    
+    // bounds socket_fd to localhost and listen on port
+    if (listener_init(&socket_fd, port) < 0) {
+        fprintf(stderr, "Invalid Port\n");
+        return 1;
+    }
+    
+    // the server runs forever until terminated by ctrl-c
+    while(1) {
+        int new_socket_fd;
+        // Blocks until a new client connection
+        new_socket_fd = listener_accept(&socket_fd);
+        bzero(buf, MAX_BUF);
+        int n = read_until(new_socket_fd, buf, MAX_BUF, "poop");
+        if (n < 0) {
+            fprintf(stderr, "can't read from socket\n");
+            return 1;
+        }
+        fprintf(stdout, "%s\n", buf);
+        close(new_socket_fd);
+
+    }
 	return 0;
 }
