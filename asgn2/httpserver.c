a150c75b3be7b8d5fcb9dd9d11bd325891e45376
Charlotte Liang <yliang82@ucsc.edu>
2023-02-05 02:36:57 -0800

    cloned helper function files from resources repo, httpserver doesn't do anything yet, Makefile successfully link helper files to httpserver

---
+++ asgn2/httpserver.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "asgn2_helper_funcs.h"
+
+int main(int argc, char **argv) {
+	return 0;
+}
