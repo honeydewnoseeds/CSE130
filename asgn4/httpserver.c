713b42cc4b6f10309bd7ad434e901c77c5db1542
Charlotte Liang <yliang82@ucsc.edu>
2023-03-13 00:45:26 -0700

    half way done, needs debugging

--- asgn4/httpserver.c
+++ asgn4/httpserver.c
@@ -10,6 +10,7 @@
 #include "request.h"
 #include "queue.h"
 
+#include <assert.h>
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -23,6 +24,7 @@
 #include <sys/stat.h>
 
 queue_t *q = NULL;
+pthread_mutex_t mutex;
 
 void *handle_connection();
 
@@ -30,7 +32,27 @@ void handle_get(conn_t *);
 void handle_put(conn_t *);
 void handle_unsupported(conn_t *);
 
+void audit(conn_t *conn, const Response_t *res) {
+
+    const Request_t *req = conn_get_request(conn);
+    char *URI = conn_get_uri(conn);
+    const char *oper = request_get_str(req);
+    uint16_t code = response_get_code(res);
+    char *id = conn_get_header(conn, "Request-Id");
+    /*
+    if (req == &REQUEST_GET) {
+        oper = "GET";
+    }else if (req == &REQUEST_PUT) {
+        oper = "PUT";
+    }else {
+        oper = "UNSUPPORTED";
+    }*/
+
+    fprintf(stderr, "%s,%s,%hu,%s\n", oper, URI, code, id);
+}
+
 int main(int argc, char **argv) {
+
     // verify the format of the arguments
     if (argc < 2) {
         warnx("wrong arguments: %s port_num", argv[0]);
@@ -49,7 +71,7 @@ int main(int argc, char **argv) {
     // default number of worker thread is 4
     int num_thread = 4;
     // if there's more than 2 arguments, check if the second argument is valid #
-    if (argc <= 3) {
+    if (argc >= 3) {
         num_thread = atoi(argv[2]);
         // if not valid #, number of worker thread default back to 4
         if (num_thread == 0) {
@@ -57,6 +79,7 @@ int main(int argc, char **argv) {
         }
     }
 
+    fprintf(stdout, "hrer1\n");
     // initializing sockets for port
     signal(SIGPIPE, SIG_IGN);
     Listener_Socket sock;
@@ -75,6 +98,11 @@ int main(int argc, char **argv) {
     for (int i = 0; i < num_thread; i++) {
         pthread_create(&threads[i], NULL, handle_connection, NULL);
     }
+
+    // initializing mutex for flock
+    int rc = 0;
+    rc = pthread_mutex_init(&mutex, NULL);
+    assert(!rc);
 
     // Listener
     while (1) {
@@ -121,7 +149,6 @@ void *handle_connection() {
                 handle_unsupported(conn);
             }
         }
-
         conn_delete(&conn);
         close(connfd);
     }
@@ -129,7 +156,7 @@ void *handle_connection() {
 }
 
 void handle_get(conn_t *conn) {
-
+    fprintf(stdout, "in get\n");
     // retrieves the URI
     char *uri = conn_get_uri(conn);
     //debug("GET request not implemented. But, we want to get %s", uri);
@@ -137,6 +164,8 @@ void handle_get(conn_t *conn) {
     // What are the steps in here?
 
     // 1. Open the file.
+    // lock
+    pthread_mutex_lock(&mutex);
     int file_fd = open(uri, O_RDONLY);
     const Response_t *res = NULL;
     // If  open it returns < 0, then use the result appropriately
@@ -145,6 +174,7 @@ void handle_get(conn_t *conn) {
     //   c. other error? -- use RESPONSE_INTERNAL_SERVER_ERROR
     // (hint: check errno for these cases)!
     if (file_fd < 0) {
+        pthread_mutex_unlock(&mutex);
         debug("%s: %d", uri, errno);
         if (errno == EACCES) {
             res = &RESPONSE_FORBIDDEN;
@@ -155,11 +185,14 @@ void handle_get(conn_t *conn) {
             res = &RESPONSE_INTERNAL_SERVER_ERROR;
         }
         conn_send_response(conn, res);
+
+        // audit
+        audit(conn, res);
         return;
     }
 
-    // locks
     flock(file_fd, LOCK_SH);
+    pthread_mutex_unlock(&mutex);
 
     // 2. Get the size of the file.
     // (hint: checkout the function fstat)!
@@ -175,6 +208,7 @@ void handle_get(conn_t *conn) {
         res = &RESPONSE_FORBIDDEN;
         conn_send_response(conn, res);
         close(file_fd);
+        audit(conn, res);
         return;
     }
 
@@ -184,6 +218,10 @@ void handle_get(conn_t *conn) {
     conn_send_file(conn, file_fd, size);
     //flock(file_fd, LOCK_UN);
     close(file_fd);
+    if (res == NULL) {
+        fprintf(stdout, "res is null\n");
+    }
+    audit(conn, res);
 }
 
 void handle_unsupported(conn_t *conn) {
@@ -191,6 +229,7 @@ void handle_unsupported(conn_t *conn) {
 
     // send responses
     conn_send_response(conn, &RESPONSE_NOT_IMPLEMENTED);
+    audit(conn, &RESPONSE_NOT_IMPLEMENTED);
 }
 
 void handle_put(conn_t *conn) {
@@ -199,26 +238,36 @@ void handle_put(conn_t *conn) {
     const Response_t *res = NULL;
     debug("handling put request for %s", uri);
 
+    // lock
+    pthread_mutex_lock(&mutex);
     // Check if file already exists before opening it.
     bool existed = access(uri, F_OK) == 0;
     debug("%s existed? %d", uri, existed);
 
     // Open the file..
-    int fd = open(uri, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+    int fd = open(uri, O_CREAT | O_WRONLY, 0600);
     if (fd < 0) {
+        // unlock
+        pthread_mutex_unlock(&mutex);
         debug("%s: %d", uri, errno);
         if (errno == EACCES || errno == EISDIR || errno == ENOENT) {
             res = &RESPONSE_FORBIDDEN;
-            conn_send_response(conn, res);
-            return;
         } else {
             res = &RESPONSE_INTERNAL_SERVER_ERROR;
-            conn_send_response(conn, res);
-            return;
         }
+        conn_send_response(conn, res);
+        audit(conn, res);
+        return;
     }
-    // lock
+
     flock(fd, LOCK_EX);
+    // unlock
+    pthread_mutex_unlock(&mutex);
+
+    // use stat to get the size of the file to truncate
+    int t = ftruncate(fd, 0);
+    assert(!t);
+    //pthread_mutex_unlock(&mutex);
     // write data from the connection to the file fd
     res = conn_recv_file(conn, fd);
 
@@ -228,6 +277,6 @@ void handle_put(conn_t *conn) {
         res = &RESPONSE_CREATED;
     }
     conn_send_response(conn, res);
-    //flock(fd, LOCK_UN);
     close(fd);
+    audit(conn, res);
 }
